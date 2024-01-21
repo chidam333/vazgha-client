dff99e5abacbe64f150d98c79f6459fa33f7ffd1
chidam <chidam3chain@gmail.com>
2024-01-22 00:22:36 +0530

    sosasd

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,23 @@
 import type {PageLoad} from './$types';
 export const load: PageLoad = async() =>{
     let date = new Date();
-    let dd = "20";
-    let mm = "08";
-    let yy = "23";
+    /*
+        let dd = `${date.getDay()}`
+    let mm = `${date.getMonth()}` 
+    let yy = `${date.getFullYear()}`
+    */
+    let dd = "17"
+    let mm = "09"
+    let yy = "23"
     let response = await fetch(`http://vazgha.chidam.xyz/api/date?date=${dd}-${mm}-${yy}`)
     console.log({response})
-    let result = await response.json()
+    let result:JSON;
+    try{
+        result = await response.json()
+    }catch(e){
+        result = {}
+        console.log({e})
+    }
     console.log({date,result})
     return{
         result
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import * as Card from "$lib/components/ui/card"
     import {toast} from "svelte-sonner";
+    import {cn} from "$lib/utils";
     import type {PageData} from "./$types";
     import {Button} from "$lib/components/ui/button";
     import {Calendar} from "$lib/components/ui/calendar";
@@ -21,16 +22,30 @@
     }
     let value : DateValue | undefined  = undefined;
     console.log({value})
+    let copyText = (e:PointerEvent)=>{
+        let spanTarget = e.target as HTMLSpanElement;
+        let text = spanTarget.innerHTML;
+        //console.log(text);
+        navigator.clipboard.writeText(text)
+        toast("messgae copied")
+    }
 </script>
 <main class="relative">
-    <div class="grid place-items-center mx-[8vh] mt-24">
+    <div class={cn("grid place-items-center mx-[8vh] mt-24F")} >
     <Carousel.Root>
     <Carousel.Content class="w-[80vw]">
+    {#if data_arr.length<=0}
+        <Card.Root >
+            <Card.Content> 
+                <span>No data found for the current date (you can try other dates)</span>
+            </Card.Content>
+        </Card.Root>
+    {/if}
     {#each data_arr as key,i}
         <Carousel.Item class="">
-            <Card.Root>
-                <Card.Content>
-                    {key}
+            <Card.Root >
+                    <Card.Content> 
+                    <span role="button" on:click={(e)=>{copyText(e)}} on:keydown={(e)=>{copyText(e)}} tabindex="0">{key}</span>
                 </Card.Content>
             </Card.Root>
         </Carousel.Item>
@@ -40,29 +55,9 @@
     <Carousel.Next />
     </Carousel.Root>
     <Popover.Root>
-        <Popover.Trigger><Button class="bg-lime-400 mt-4">change date</Button></Popover.Trigger>
+        <Popover.Trigger asChild let:builder><Button class="bg-lime-400 mt-4" builders={[builder]}>change date</Button></Popover.Trigger>
         <Popover.Content><Calendar bind:value initialFocus/></Popover.Content>
     </Popover.Root>
-    <!--
-    <Carousel.Root class="">
-      <Carousel.Content class="">
-        {#each data_arr as key,i}
-          <Carousel.Item >
-            <button class="allunset" on:keydown={()=>toast("copied")} on:click={()=>{toast("copied")}}>
-              <Card.Root >
-                <Card.Content 
-                  class=""
-                >
-                  <span class="" >{key}</span>
-                </Card.Content>
-              </Card.Root>
-            </button>
-          </Carousel.Item>
-        {/each}
-      </Carousel.Content>
-      <Carousel.Previous />
-      <Carousel.Next />
-    </Carousel.Root>-->
     </div>
 </main>
 <style>
